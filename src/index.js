0ddb94a0d1c4f2fc6d779a2af3a75e8081d83136
Anton Pushkarev <adpushkarev@yandex.ru>
2025-11-12 19:38:36 +0300

    Init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+  import { DateTime } from 'luxon';
+
+  setInterval(() => {
+    hh.textContent = DateTime
+    .local()
+    .setLocale('ru')
+    .toFormat('dd.LL.y HH:mm:ss');
+  }, 1000);
+   
